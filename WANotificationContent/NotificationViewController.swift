487439911d7eef1b1e90d26633cd4e2e4bfa319a
PrettyAndroid <rejoylin2015@gmail.com>
2018-03-23 13:09:18 +0530

    Notification added

---
+++ WANotificationContent/NotificationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationViewController.swift
+//  WANotificationContent
+//
+//  Created by Walinns Innovation on 23/03/18.
+//  Copyright © 2018 Walinns Innovation. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import UserNotificationsUI
+
+class NotificationViewController: UIViewController, UNNotificationContentExtension {
+
+    @IBOutlet var label: UILabel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any required interface initialization here.
+    }
+    
+    func didReceive(_ notification: UNNotification) {
+        self.label?.text = notification.request.content.body
+    }
+
+}
